f6151cca9184e0ef22655d8f40b298db35532465
JoeMc131 <joemc131@outlook.com>
2026-03-29 17:49:00 +0100

    adding uk garage

--- Joe_examples.py
+++ Joe_examples.py
@@ -5,7 +5,7 @@ import streamlit as st
 main_pg = st.Page('./pages/main.py', title='Home')
 tornado_pg = st.Page('./pages/earthquakes.py', title='Earthquakes')
 pokemon_pg = st.Page('./pages/Pokemon.py', title='Shiny Pokemon')
-uk_garage_pg = st.Page('./pages/UK_garage', title='UK garage')
+uk_garage_pg = st.Page('./pages/UK_garage.py', title='UK garage')
 
 pg = st.navigation([main_pg, tornado_pg, pokemon_pg, uk_garage_pg])
 
